3a237c35bd7c6c56d3d2b21d04b1bbb49af585c7
Jansen Tan <jansenmtan@gmail.com>
2021-07-08 12:35:09 -0500

    update `aslrtest.c`
    include test for deeply called functions

--- user/aslrtest.c
+++ user/aslrtest.c
@@ -3,6 +3,23 @@
 
 int y;
 
+void
+f2()
+{
+  int x;
+  x = 3;
+  printf("f2: stack: &x: %p\n", &x);
+}
+
+void
+f1()
+{
+  int x;
+  x = 2;
+  printf("f1: stack: &x: %p\n", &x);
+  f2();
+}
+
 int main(int argc, char *argv[])
 {
   int x ;
@@ -10,9 +27,10 @@ int main(int argc, char *argv[])
   x = 10;
   y = 20;
   ptr = (int *)malloc(4);
-  printf("main! :%p\n", main);
-  printf("Stack : x address! :%p\n", &x);
-  printf("Stack y: address! :%p\n", &y);
-  printf("heap address!: %p\n", ptr);
-  exit(1);
+  printf("main:      %p \n", main);
+  printf("stack: &x: %p \n", &x);
+  printf("stack: &y: %p \n", &y);
+  printf("heap: ptr: %p \n", ptr);
+  f1();
+  exit(0);
 }
