1cc02d0486ec2f066b6e4a3cbf9678eef50be879
Jansen Tan <jansenmtan@gmail.com>
2021-07-17 13:52:35 -0500

    x

--- user/aslrtest.c
+++ user/aslrtest.c
@@ -34,6 +34,8 @@ int main(int argc, char *argv[])
   printf("&x - main:   %p \n", &x - (int *)main);
   printf("&y - main:   %p \n", &y - (int *)main);
   printf("heap: ptr:   %p \n", ptr);
+  printf("ptr - &x:    %p \n", ptr - &x);
+  printf("ptr - &y:    %p \n", ptr - &y);
   f1();
   printf("exec - main: %p \n", main);
   exit(0);
